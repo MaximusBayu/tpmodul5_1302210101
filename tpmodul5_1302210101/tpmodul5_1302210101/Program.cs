4724ad827fd96a00394719282e0872b4aaa1eae1
MaximusBayu <121425228+MaximusBayu@users.noreply.github.com>
2023-03-20 03:50:42 +0700

    Tambahan HaloGeneric
    menambahkan class HaloGeneric dengan method SapaUser

---
+++ tpmodul5_1302210101/tpmodul5_1302210101/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+using tpmodul5_1302210101;
+
+HaloGeneric<string> haloGeneric = new HaloGeneric<string>();
+
+string X = Console.ReadLine();
+haloGeneric.sapaUser(X);
